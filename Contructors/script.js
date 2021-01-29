96a992e878e591bf3884c7eeed5b15f95193b7c7
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-01-29 12:44:59 -0500

    Add files via upload

---
+++ Contructors/script.js
@@ -0,0 +1,17 @@
+
+function dog(ownersName, dogsName, color) {
+    this.ownersName = ownersName;
+    this.dogsName = dogsName;
+    this.color = color;
+    this.numLegs = 4;
+}
+
+let terrier = new dog("Tom", "J", "Blue");
+console.log(terrier.ownersName + " has a dog named " + terrier.dogsName + " and it has blue fur")
+
+let retriever = new dog("Jerry", "Hanna", "yellow");
+console.log(retriever.ownersName + " has a dog named " + retriever.dogsName + " and its fur is yellow")
+
+
+
+
